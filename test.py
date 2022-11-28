1ddf879c5cc79b9b2143d9af6c6f0d967f96b487
Lekan-at-244939208359 <lekan@parket.co.za>
2022-11-28 19:18:51 +0200

    minor update

--- test.py
+++ test.py
@@ -2,3 +2,12 @@ from git import Repo
 repo = Repo()
 changedFiles = [ item.a_path for item in repo.index.diff(None) ]
 print(changedFiles, repo)
+from git import Repo
+
+# Suppose the current path is the root of the repository
+r = Repo('.')
+o = r.git.show('HEAD', pretty="", name_only=True)
+print(o)
+# print(dir(repo))
+print(repo.untracked_files, 11)
+print(repo.working_dir)
