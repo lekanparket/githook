04970e1b10a648a4908b5684f2c45783eb97e218
Lekan-at-244939208359 <lekan@parket.co.za>
2022-12-03 18:11:18 +0200

    min

--- test.py
+++ test.py
@@ -2,16 +2,18 @@ from git import Repo
 repo = Repo()
 changedFiles = [ item.a_path for item in repo.index.diff(None) ]
 print(changedFiles,)
-from git import Repo
 
+
+from git import Repo
 # Suppose the current path is the root of the repository
 r = Repo('.')
 o = r.git.show('HEAD', pretty="", name_only=True)
-print(o)
+print(o, 343)
 # print(dir(repo))
 
 print(repo.untracked_files, 11)
 print(repo.working_dir)
 # python .git/hooks/post-commit.py
+
 
 print('TESTTTTI')
