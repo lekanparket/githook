2fb80b155af92fb34127eb3608bbc0ca4ee4ad23
Lekan-at-244939208359 <lekan@parket.co.za>
2022-12-03 18:49:33 +0200

    minor

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ print(changedFiles,)
 from git import Repo
 # Suppose the current path is the root of the repository
 r = Repo('.')
-o = r.git.show('HEAD',)
+o = r.git.show('HEAD', pretty="", )
 diff = repo.git.diff('HEAD~1..HEAD', name_only=True)
 print(diff, 124)
 print(o)
