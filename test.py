eb8392f97a99e6adc59c3d02a29f6cb651091a46
Lekan-at-244939208359 <lekan@parket.co.za>
2022-12-03 18:51:36 +0200

    minor

--- test.py
+++ test.py
@@ -3,16 +3,16 @@ repo = Repo()
 changedFiles = [ item.a_path for item in repo.index.diff(None) ]
 print(changedFiles,)
 
-from git import Repo
-# Suppose the current path is the root of the repository
-r = Repo('.')
-o = r.git.show('HEAD', name_only=True)
-diff = repo.git.diff('HEAD~1..HEAD', name_only=True)
-print(diff, 124)
-print(o)
-# print(dir(repo))
-print(repo.untracked_files, 11)
-print(repo.working_dir)
+# from git import Repo
+# # Suppose the current path is the root of the repository
+# r = Repo('.')
+# o = r.git.show('HEAD', name_only=True)
+# diff = repo.git.diff('HEAD~1..HEAD', name_only=True)
+# print(diff, 124)
+# print(o)
+# # print(dir(repo))
+# print(repo.untracked_files, 11)
+# print(repo.working_dir)
 # python .git/hooks/post-commit.py
 print('TEST#######')
 
