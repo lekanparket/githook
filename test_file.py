7077a55e3e665d564639127b0cb575166696d485
Lekan-at-244939208359 <lekan@parket.co.za>
2023-01-13 17:40:37 +0200

    minor update

--- test_file.py
+++ test_file.py
@@ -59,5 +59,6 @@ if __name__ == '__main__':
     temp = Temperature(country='usa', city='san francisco')
     print(temp.get())
 
+
 SECRET_KEY = 'django-insecure-^k^8ce44l(&^%gbh$361$xgp@dixlh#!%7279^vuk9*v454-s6'
 rr = 'AKIATSB4DW2EQFDMJEMV'
