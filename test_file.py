6aee75d412b3119845675cba49ebaff331a4561d
Lekan-at-244939208359 <lekan@parket.co.za>
2023-04-07 17:25:37 +0200

    lolrf

--- test_file.py
+++ test_file.py
@@ -43,22 +43,8 @@ class Temperature:
         scraped_content = self._scrape()
         return float(scraped_content['temp'].replace('°C', '').strip())
 
-"""
-headers = {
-    'pragma': 'no-cache',
-    'cache-control': 'no-cache',
-    'dnt': '1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-}
-"""
-
 if __name__ == '__main__':
     temp = Temperature(country='usa', city='san francisco')
     print(temp.get())
 
-
 rr = 'AKIATSB4DW2EQFDMJEMV'
-        
